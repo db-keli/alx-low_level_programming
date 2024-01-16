2c69778c77dc01ffb6e4fd38673a75baf1fae85d
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-01-16 04:40:40 +0000

    make me win

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /tmp/win.so https://github.com/Aaron-Ontoyin/alx-low_level_programming/blob/3ba0105be190790d69213c41a1f0143ed9731285/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
