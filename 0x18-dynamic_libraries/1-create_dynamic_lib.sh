b7f4c0bd2f8a5df3d95127b46c592830ac4360c2
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-01-20 03:21:25 +0000

    fixed

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c -Wno-return-type
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c -Wno-return-type -Wno-unused-parameter
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
